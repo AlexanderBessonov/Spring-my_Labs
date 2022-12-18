4eb5c3f857027cf4cd37c9258d872c14d983dd60
Alexander Bessonov <bessonovUSA@gmail.com>
2022-12-17 20:06:02 -0600

    lab - 08 Discount

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/OrderController.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/OrderController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/v1/order")
 public class OrderController {
 
     private final OrderService orderService;
